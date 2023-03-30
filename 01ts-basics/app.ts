1fe89ad9e40eaf59676626b5f3e980f5115029ee
Bryan C Guner <bryan.guner@gmail.com>
2023-03-30 19:39:29 -0400

    changed combine logic

--- 01ts-basics/app.ts
+++ 01ts-basics/app.ts
@@ -4,16 +4,13 @@ function combine(
   resultConversion: string
 ) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+    return result;
+
 }
 
 const combineAges = combine(30, 26, "as-number");
