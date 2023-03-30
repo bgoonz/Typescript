e3755cef065eb140e13c6da5104d68393c9ba1b5
Bryan C Guner <bryan.guner@gmail.com>
2023-03-30 19:35:04 -0400

    literal types first approach... do calculation and then convert result to desired output type

--- 01ts-basics/app.ts
+++ 01ts-basics/app.ts
@@ -1,16 +1,26 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: string
+) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-
-  return result;
+  if (resultConversion === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Bryan", "Anna");
+const combineStringAges = combine("30", "26", "as-number");
+console.log(combineStringAges);
+
+const combineNames = combine("Bryan", "Anna", "as-text");
 console.log(combineNames);
