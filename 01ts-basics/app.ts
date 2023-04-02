e2b4c611281a1819ee7198d79d1b44a16a090b28
Bryan C Guner <bryan.guner@gmail.com>
2023-04-02 17:08:35 -0400

    28 function types and callbacks

--- 01ts-basics/app.ts
+++ 01ts-basics/app.ts
@@ -1,19 +1,29 @@
 function add(n1: number, n2: number): number {
-    return n1 + n2;
+  return n1 + n2;
 }
-
 
 function printResult(num: number): void {
-    console.log("Result: " + num);
-    //return type is void because it doesn't return anything
+  console.log("Result: " + num);
+  //return type is void because it doesn't return anything
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+    
+
+
 printResult(add(5, 12));
-console.log(printResult(add(5, 12)));//undefined
+console.log(printResult(add(5, 12))); //undefined
 
-
-let combineValues: (a:number,b:number) =>number;
+let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 
 console.log(combineValues(8, 8));
+
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
---
+++ 01ts-basics/functions-as-types.ts
@@ -0,0 +1,19 @@
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+
+function printResult(num: number): void {
+    console.log("Result: " + num);
+    //return type is void because it doesn't return anything
+}
+
+printResult(add(5, 12));
+console.log(printResult(add(5, 12)));//undefined
+
+
+let combineValues: (a:number,b:number) =>number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
