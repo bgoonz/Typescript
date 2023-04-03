6b01408006756539f906791c08a4cae66b6ce9c2
Bryan C Guner <bryan.guner@gmail.com>
2023-04-03 13:31:02 -0400

    arrow functions with auto html logger

--- 04next-gen-js-and-ts/src/app.ts
+++ 04next-gen-js-and-ts/src/app.ts
@@ -18,4 +18,14 @@ const add = (a: number, b: number) => {
   return a + b;
 };
 
-console.log(add(2, 5));
+const printOutput = (output: string | number) => {
+    console.log(output);
+}
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5, 2));
